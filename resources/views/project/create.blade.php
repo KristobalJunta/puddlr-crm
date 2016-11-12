2218bea21dc60aded222be0f43fc788302ce4a23
Kristobal <junta.kristobal@gmail.com>
2016-11-12 16:23:20 +0200

    project fom style fix

--- resources/views/project/create.blade.php
+++ resources/views/project/create.blade.php
@@ -8,31 +8,33 @@
 
     <section class="main-container">
         @include('blocks.header')
-        <form action="/app/{{ $team->slug }}/project" method="post" class="project-form">
-            {!! csrf_field() !!}
+        <div class="project-form-wrap">
+            <form action="/app/{{ $team->slug }}/project" method="post" class="project-form">
+                {!! csrf_field() !!}
 
-            <label for="">О проекте</label>
-            <input type="text" name="name" placeholder="Название">
+                <label for="">О проекте</label>
+                <input type="text" name="name" placeholder="Название">
 
-            <textarea name="description" placeholder="Описание"></textarea>
+                <textarea name="description" placeholder="Описание"></textarea>
 
-            <select name="project_template_id">
-                @foreach ($templates as $template)
-                    <option value="{{ $template->id }}">{{ $template->name }}</option>
+                <select name="project_template_id">
+                    @foreach ($templates as $template)
+                        <option value="{{ $template->id }}">{{ $template->name }}</option>
+                    @endforeach
+                </select>
+
+                @foreach($users as $member)
+                    <div class="project-form-user">
+                        Квота рабочего времени для {{ $member->name }} ({{ $member->role->name }})
+                        <input type="text" name="quotas[{{ $member->id }}]" value="" placeholder="(в часах)">
+                    </div>
                 @endforeach
-            </select>
 
-            @foreach($users as $member)
-                <div class="project-form-user">
-                    Квота рабочего времени для {{ $member->name }} ({{ $member->role->name }})
-                    <input type="text" name="quotas[{{ $member->id }}]" value="" placeholder="(в часах)">
-                </div>
-            @endforeach
-
-            <button type="submit" class="project-form__submit">
-                Создать
-            </button>
-        </form>
+                <button type="submit" class="project-form__submit">
+                    Создать
+                </button>
+            </form>
+        </div>
     </section>
 
 @endsection
