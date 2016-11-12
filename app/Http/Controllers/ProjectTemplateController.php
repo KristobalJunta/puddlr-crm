a7e65a0c34da4a11d8f8cfc7cd44412dbc0dd873
andcrow <andcrow95@gmail.com>
2016-11-12 12:35:27 +0200

    as is #5

--- app/Http/Controllers/ProjectTemplateController.php
+++ app/Http/Controllers/ProjectTemplateController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Team;
+use App\Status;
+
 use App\ProjectTemplate;
 
 class ProjectTemplateController extends Controller
@@ -68,6 +70,7 @@ class ProjectTemplateController extends Controller
             'team' => $team,
             'user' => $user,
             'template' => $projectTemplate,
+            'statuses' => Status::all()
         ]);
     }
 
